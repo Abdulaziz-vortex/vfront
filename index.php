ce8d85bafdacaa319c1e28ad4958f2b6226228fc
Vortex <abdulaziz.mahmudov1999@gmail.com>
2021-01-21 13:59:01 +0500

    update

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>project</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		
+		<!-- Header section -->
+		
+		<header class="header">
+			<div class="container">
+				<div class="header__row">
+					<div class="header__logo">Vortex we desig ad asdasd</div>
+					<nav class="header__menu">
+						<ul class="header__ul">
+							<li class="header__li"><a href="#" class="header__link">Home</a></li>
+							<li class="header__li"><a href="#" class="header__link">News</a></li>
+							<li class="header__li"><a href="#" class="header__link">Blogs</a></li>
+							<li class="header__li"><a href="#" class="header__link">About us</a></li>
+							<li class="header__li"><a href="#" class="header__link">Contacts</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		
+		<!-- Content section here -->
+		
+		<div class="content">
+			<div class="container">Content here</div>
+		</div>
+		
+		<!-- Footer section here -->
+		
+		<footer class="footer">
+			<div class="container">
+				<div class="footer__row">
+					<div class="footer__text">Vortex web-development - 2021</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
